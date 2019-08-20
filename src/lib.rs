a5a4c89363e5dd6ce1c9f6b37b79c6fb6b5c2920
Mackenzie Clark <mackenzie.a.z.c@gmail.com>
2019-08-19 18:43:04 -0700

    remove nightly feature flags

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,3 @@
-#![feature(const_fn)]
-#![feature(unboxed_closures)]
-#![feature(fn_traits)]
-#![feature(clamp)]
-#![feature(panic_info_message)]
-
 #[macro_use]
 extern crate log;
 extern crate env_logger;
