3f4fae0ff968e0610a4df68573d441fe12014d41
Mahmut Bulut <vertexclique@gmail.com>
2019-10-24 22:26:51 +0200

    Expose catch_unwind

--- lightproc/src/lib.rs
+++ lightproc/src/lib.rs
@@ -1,20 +1,22 @@
-mod catch_unwind;
 mod layout_helpers;
 mod proc_data;
-mod proc_ext;
 mod proc_layout;
 mod proc_vtable;
 mod raw_proc;
 mod state;
 
 pub mod lightproc;
+pub mod catch_unwind;
 pub mod proc_handle;
-pub mod recoverable_handle;
+pub mod proc_ext;
 pub mod proc_stack;
+pub mod recoverable_handle;
 
 pub mod prelude {
     pub use crate::lightproc::*;
+    pub use crate::catch_unwind::*;
+    pub use crate::proc_ext::*;
     pub use crate::proc_handle::*;
-    pub use crate::recoverable_handle::*;
     pub use crate::proc_stack::*;
+    pub use crate::recoverable_handle::*;
 }
