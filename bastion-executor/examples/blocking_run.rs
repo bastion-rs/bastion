a748b8c05daedbb89b2700028aacb355de85956b
Mahmut Bulut <vertexclique@gmail.com>
2019-10-30 22:09:53 +0100

    Better messages for example

--- bastion-executor/examples/blocking_run.rs
+++ bastion-executor/examples/blocking_run.rs
@@ -4,8 +4,8 @@ use lightproc::proc_stack::ProcStack;
 fn main() {
     run(
         async {
-            println!("DATA");
-            panic!("kaka");
+            println!("Example execution");
+            panic!("fault");
         },
         ProcStack::default().with_after_panic(|| println!("after panic")),
     );
