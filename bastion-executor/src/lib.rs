566c4080566cb34b457f1b7ae4442738b7682c4d
Mahmut Bulut <vertexclique@gmail.com>
2019-11-03 00:45:25 +0100

    Add benchmarks

--- bastion-executor/src/lib.rs
+++ bastion-executor/src/lib.rs
@@ -8,9 +8,11 @@
 #![feature(asm)]
 // Allocator features
 #![feature(allocator_api)]
+#![feature(extern_types)]
 #![feature(core_intrinsics)]
 #![feature(libstd_sys_internals)]
 #![feature(thread_local)]
+#![feature(const_fn)]
 // Force missing implementations
 #![warn(missing_docs)]
 #![warn(missing_debug_implementations)]
