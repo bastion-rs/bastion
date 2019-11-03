2cca1308df707dde68523e090c59f47433a893d1
Mahmut Bulut <vertexclique@gmail.com>
2019-11-03 22:08:29 +0100

    Add features back

--- bastion-executor/src/lib.rs
+++ bastion-executor/src/lib.rs
@@ -4,6 +4,13 @@
 //! NUMA-aware SMP based Fault-tolerant Executor
 //!
 
+// Allocator features
+#![feature(allocator_api)]
+#![feature(extern_types)]
+#![feature(core_intrinsics)]
+#![feature(libstd_sys_internals)]
+#![feature(thread_local)]
+#![feature(const_fn)]
 // Force missing implementations
 #![warn(missing_docs)]
 #![warn(missing_debug_implementations)]
@@ -23,5 +30,4 @@ pub mod worker;
 pub mod prelude {
     pub use crate::pool::*;
     pub use crate::run::*;
-    pub use crate::allocator::*;
 }
