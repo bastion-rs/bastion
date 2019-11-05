2497485d49f601896c3a509b913d84163a1c40db
Matthieu Le brazidec <matthieu@lebrazidec.email>
2019-11-06 00:53:25 +0100

    Added documentation for Bastion.

--- bastion/src/bastion.rs
+++ bastion/src/bastion.rs
@@ -5,6 +5,131 @@ use crate::supervisor::{Supervisor, SupervisorRef};
 use crate::system::{ROOT_SPV, SYSTEM, SYSTEM_SENDER};
 use std::thread;
 
+/// A `struct` allowing to access the system's API to initialize it,
+/// start, stop and kill it and to create new supervisors and top-level
+/// children groups.
+///
+/// # Example
+///
+/// ```
+/// use bastion::prelude::*;
+///
+/// fn main() {
+///     // Initializing the system (this is required)...
+///     Bastion::init();
+///
+///     // Starting the system...
+///     Bastion::start();
+///
+///     // Creating a new supervisor...
+///     let supervisor = Bastion::supervisor(|sp| {
+///         sp
+///         // ...with a specific supervision strategy...
+///             .with_strategy(SupervisionStrategy::OneForAll)
+///         // ...and some supervised children groups...
+///             .children(|children| {
+///                 // ...
+///                 # children
+///             })
+///             .children(|children| {
+///                 // ...
+///                 # children
+///             })
+///         // ...or even supervised supervisors...
+///             .supervisor(|sp| {
+///                 // ...
+///                 # sp
+///             })
+///     }).expect("Couldn't create the supervisor.");
+///
+///     // ...which can start supervising new children groups
+///     // later on...
+///     supervisor.children(|children| {
+///         // ...
+///         # children
+///     }).expect("Couldn't create the supervised children group.");
+///
+///     // ...or broadcast messages to all its supervised children
+///     // and supervisors...
+///     supervisor.broadcast("A message containing data.").expect("Couldn't broadcast the message.");
+///
+///     // ...and then can even be stopped or killed...
+///     supervisor.stop().expect("Couldn't stop the supervisor");
+///     // supervisor.kill().expect("Couldn't kill the supervisor");
+///
+///     // Creating a new top-level children group...
+///     let children = Bastion::children(|children| {
+///         children
+///         // ...containing a defined number of elements...
+///             .with_redundancy(4)
+///         // ...all executing a similar future...
+///             .with_exec(|ctx: BastionContext| {
+///                 async move {
+///                     // ...receiving and matching messages...
+///                     msg! { ctx.recv().await?,
+///                         ref msg: &'static str => {
+///                             // ...
+///                         };
+///                         msg: &'static str => {
+///                             // ...
+///                         };
+///                         msg: &'static str =!> {
+///                             // ...
+///                         };
+///                         // ...
+///                         _: _ => ();
+///                     }
+///
+///                     // ...
+///
+///                     Ok(())
+///                 }
+///             })
+///     }).expect("Couldn't create the children group.");
+///
+///     // ...which can broadcast messages to all its elements...
+///     children.broadcast("A message containing data.").expect("Couldn't broadcast the message.");
+///
+///     // ...and then can even be stopped or killed...
+///     children.stop().expect("Couldn't stop the children group.");
+///     // children.kill().expect("Couldn't kill the children group.");
+///
+///     // Create a new top-level children group and getting a list
+///     // of reference to its elements...
+///     let children = Bastion::children(|children| {
+///         // ...
+///         # children
+///     }).expect("Couldn't create the children group.");
+///     let elems: &[ChildRef] = children.elems();
+///
+///     // ...to then get one of its elements' reference...
+///     let child = &elems[0];
+///
+///     // ...to then "tell" it messages...
+///     child.tell("A message containing data.").expect("Couldn't send the message.");
+///
+///     // ...or "ask" it messages...
+///     let answer: Answer = child.ask("A message containing data.").expect("Couldn't send the message.");
+///     # async {
+///     // ...until the child eventually answers back...
+///     let answer: Result<Msg, ()> = answer.await;
+///     # };
+///
+///     // ...and then even stop or kill it...
+///     child.stop().expect("Couldn't stop the child.");
+///     // child.kill().expect("Couldn't kill the child.");
+///
+///     // Broadcasting a message to all the system's children...
+///     Bastion::broadcast("A message containing data.").expect("Couldn't send the message.");
+///
+///     // Stopping or killing the system...
+///     Bastion::stop();
+///     // Bastion::kill();
+///
+///     // Blocking until the system has stopped (or got killed)...
+///     Bastion::block_until_stopped();
+/// }
+/// ```
 pub struct Bastion {
     _priv: (),
 }
@@ -31,7 +156,8 @@ impl Bastion {
     /// }
     /// ```
     pub fn init() {
-        //std::panic::set_hook(Box::new(|_| ()));
+        // NOTE: this hides all panic messages
+        std::panic::set_hook(Box::new(|_| ()));
 
         // NOTE: this is just to make sure that SYSTEM_SENDER has been initialized by lazy_static
         SYSTEM_SENDER.is_closed();
