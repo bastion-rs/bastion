98be08bde195d6c7b4b9197b2446bfbf381bbc64
Mahmut Bulut <vertexclique@gmail.com>
2019-11-28 04:11:52 +0100

    Fix redundancy lower bounds

--- bastion/tests/prop_children_redundancy.rs
+++ bastion/tests/prop_children_redundancy.rs
@@ -3,7 +3,8 @@ use proptest::prelude::*;
 
 proptest! {
     #[test]
-    fn proptest_redundancy(r in std::usize::MIN..std::usize::MAX) {
+    fn proptest_redundancy(r in std::usize::MIN..5_000) {
+        dbg!(r);
         Bastion::children(|children| {
             children
                 // shrink over the redundancy
