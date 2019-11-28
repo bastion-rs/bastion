ac8219a4d54c92cc27aad96e7fc51cf1ab8ce68d
Mahmut Bulut <vertexclique@gmail.com>
2019-11-28 04:15:03 +0100

    Remove debug

--- bastion/tests/prop_children_redundancy.rs
+++ bastion/tests/prop_children_redundancy.rs
@@ -4,7 +4,6 @@ use proptest::prelude::*;
 proptest! {
     #[test]
     fn proptest_redundancy(r in std::usize::MIN..5_000) {
-        dbg!(r);
         Bastion::children(|children| {
             children
                 // shrink over the redundancy
