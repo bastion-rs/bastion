c7b64b7551f4cd980bf2458babc5fb2a7ce06698
Valeryi Savich <Relrin78@gmail.com>
2020-01-21 18:41:30 +0200

    Added example for the supervisor with recovery strategy

---
+++ bastion/examples/restart_strategy.rs
@@ -0,0 +1,31 @@
+use std::time::Duration;
+
+use bastion::prelude::*;
+
+fn main() {
+    Bastion::init();
+
+    Bastion::supervisor(supervisor).expect("Couldn't create the supervisor.");
+
+    Bastion::start();
+    Bastion::block_until_stopped();
+}
+
+fn supervisor(supervisor: Supervisor) -> Supervisor {
+    let restart_strategy = RestartStrategy::default()
+        .with_max_restarts(Some(3))
+        .with_actor_restart_strategy(ActorRestartStrategy::LinearBackOff {
+            timeout: Duration::from_secs(1),
+        });
+
+    supervisor
+        .with_restart_strategy(restart_strategy)
+        .children(|children| failed_actors_group(children))
+}
+
+fn failed_actors_group(children: Children) -> Children {
+    children.with_exec(move |ctx: BastionContext| async move {
+        println!("Worker started!");
+        panic!("Unexpected error...");
+    })
+}
