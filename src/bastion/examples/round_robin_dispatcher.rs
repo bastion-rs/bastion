0412bb76b9ae73d5e80b0e416e050e9221e85809
oScape <benoit.chassignol@gmail.com>
2020-06-24 13:19:59 +0200

    it works

--- src/bastion/examples/round_robin_dispatcher.rs
+++ src/bastion/examples/round_robin_dispatcher.rs
@@ -8,46 +8,59 @@ fn main() {
     env_logger::init();
     // We need bastion to run our program
     Bastion::init();
+
+    Bastion::supervisor(caller_supervisor)
+        .and_then(|_| Bastion::supervisor(rounder_supervisor))
+        .expect("Couldn't create supervisor chain.");
+
     Bastion::start();
-
-    Bastion::children(|children: Children| {
-        children
-            .with_name("dispatcher")
-            .with_redundancy(5)
-            .with_dispatcher(Dispatcher::with_type(DispatcherType::Named("Roudner".to_string())))
-            .with_exec(move |ctx: BastionContext| {
-                async move {
-                    msg! { 
-                        ctx.recv().await?,
-                        t: &'static str =!> {
-                            println!("Received {} str", t);
-                            answer!(ctx, t).expect("Couldn't send an answer.");
-                        };
-                        _: _ => ();
-                    }
-
-                    Ok(())
-                }
-            })
-    }).expect("Couldn't start the dispatcher children group");
-
-    let test: Vec<&'static str> = vec!["test1","test2","test3","test4"];
-
-    Bastion::children(|children: Children| {
-        children
-            .with_exec(move |ctx: BastionContext| {
-                let test = test.clone();
-                async move {
-                    let target = BroadcastTarget::Group("dispatcher".to_string());
-                    for t in test {
-                        ctx.broadcast_message(target.clone(), t);
-                    }
-                    Bastion::stop();
-
-                    Ok(())
-                }
-            })
-    }).expect("Couldn't start a new children group");
-
     Bastion::block_until_stopped();
+}
+
+fn rounder_supervisor(supervisor: Supervisor) -> Supervisor {
+    supervisor.children(|children| rounder_group(children))
+}
+
+fn caller_supervisor(supervisor: Supervisor) -> Supervisor {
+    supervisor.children(|children| caller_group(children))
+}
+
+fn rounder_group(children: Children) -> Children {
+    children
+        .with_redundancy(5)
+        .with_dispatcher(Dispatcher::with_type(DispatcherType::Named("Rounder".to_string())))
+        .with_exec(move |ctx: BastionContext| {
+            async move {
+                msg! {
+                    ctx.recv().await?,
+                    raw_message: Arc<SignedMessage> => {
+                        let message = Arc::try_unwrap(raw_message).unwrap();
+                        msg! { message,
+                            ref data: &str => {
+                                println!("Received {}", data);
+                            };
+                            _: _ => ();
+                        }
+                    };
+                    _: _ => ();
+                }
+
+                Ok(())
+            }
+        })
+}
+
+fn caller_group(children: Children) -> Children {
+    children
+        .with_exec(move |ctx: BastionContext| {
+            async move {
+                let test: Vec<&str> = vec!["test1","test2","test3","test4","test5"];
+                let target = BroadcastTarget::Group("Rounder".to_string());
+                for t in test {
+                    ctx.broadcast_message(target.clone(), t);
+                }
+                Bastion::stop();
+                Ok(())
+            }
+        })
 }
