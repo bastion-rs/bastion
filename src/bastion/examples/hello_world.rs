a8bcff2d8276d2f32c83301c3c32f856f6106fa8
oScape <benoit.chassignol@gmail.com>
2020-07-05 23:29:07 +0200

    ask message

--- src/bastion/examples/hello_world.rs
+++ src/bastion/examples/hello_world.rs
@@ -7,22 +7,34 @@ use bastion::prelude::*;
 fn main() {
     // We need bastion to run our program
     Bastion::init();
-    // We are starting the Bastion program now
     Bastion::start();
-    // We are creating the group of children which will receive the message
-    let children = Bastion::children(|children| {
+    // We are creating a group of children
+    Bastion::children(|children| {
         // We are creating the function to exec
         children.with_exec(|ctx: BastionContext| {
             async move {
+                // We are creating the asker
+                let answer = ctx
+                    // It sending a message to the current child and return the Answer
+                    .ask(&ctx.current().addr(), "hello, world!")
+                    .expect("Couldn't send the message.");
+                // We are defining a behavior when a msg is received
                 msg! {
                   // We are waiting a msg
                   ctx.recv().await?,
-                  ref msg: &'static str => {
-                    // We are logging the broadcasted msg below
-                    println!("{}", msg);
+                  // We are catching a msg
+                  msg: &'static str =!> {
+                    // We are answering to a msg
+                    let _ = answer!(ctx, msg);
                   };
                   _: _ => ();
                 }
+                // We are waiting the answer
+                let (msg, _) = answer.await?.extract();
+                // We are casting the received message into it waiting type
+                let msg: &str = msg.downcast().unwrap();
+                // We are printing the message
+                println!("{}", msg);
                 // We are stopping bastion here
                 Bastion::stop();
                 Ok(())
@@ -30,10 +42,6 @@ fn main() {
         })
     })
     .expect("Couldn't create the children group.");
-    // We are creating the message to broadcast to the children group
-    children
-        .broadcast("Hello, world!")
-        .expect("Couldn't broadcast the message.");
     // We are waiting until the Bastion has stopped or got killed
     Bastion::block_until_stopped();
 }
