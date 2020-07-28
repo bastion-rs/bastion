be0c99fb8f15c254d4002965adaf2aa32e38ba99
o0Ignition0o <jeremy.lempereur@gmail.com>
2020-07-29 00:21:25 +0200

    couple of examples left...

--- src/bastion/examples/tcp-servers.rs
+++ src/bastion/examples/tcp-servers.rs
@@ -1,10 +1,51 @@
 use bastion::prelude::*;
+#[cfg(not(target_os = "windows"))]
 use futures::io;
-use std::net::{TcpListener, TcpStream};
+use std::net::{TcpListener, TcpStream, ToSocketAddrs};
 use std::sync::atomic::{AtomicUsize, Ordering};
 
+#[cfg(not(target_os = "windows"))]
+async fn run(addr: impl ToSocketAddrs) -> io::Result<()> {
+    let listener = Handle::<TcpListener>::bind(addr).unwrap();
+    println!("Listening on {}", listener.get_ref().local_addr().unwrap());
+
+    // Accept clients in a loop.
+    loop {
+        let (stream, peer_addr) = listener.accept().await.unwrap();
+        println!("Accepted client: {}", peer_addr);
+
+        // Spawn a task that echoes messages from the client back to it.
+        spawn(echo(stream));
+    }
+    Ok(())
+}
+
+#[cfg(target_os = "windows")]
+async fn run(addr: impl ToSocketAddrs) -> io::Result<()> {
+    let listener = std::net::TcpListener::bind(addr).unwrap();
+    println!("Listening on {}", listener.local_addr().unwrap());
+
+    // Accept clients in a loop.
+    while let Ok(stream) = listener.incoming() {
+        println!("Accepted client");
+        // Spawn a task that echoes messages from the client back to it.
+        spawn(echo(stream));
+    }
+    Ok(())
+}
+
+#[cfg(not(target_os = "windows"))]
 async fn echo(stream: Handle<TcpStream>) -> io::Result<()> {
     io::copy(&stream, &mut &stream).await?;
+    Ok(())
+}
+
+#[cfg(target_os = "windows")]
+async fn echo(stream: TcpStream) -> io::Result<()> {
+    let mut buf = [0 as u8; 256];
+    while let Ok(size) = stream.read(&mut buf) {
+        stream.write(&buf[0..size]).unwrap();
+    }
     Ok(())
 }
 
@@ -17,7 +58,8 @@ static TCP_SERVERS: AtomicUsize = AtomicUsize::new(TCP_SERVER_COUNT);
 /// Prologue:
 ///
 /// This example demonstrates using 10 parallel tcp servers
-
+/// non windows versions use io::copy from nuclei
+/// windows versions use a regular stream copy
 fn main() {
     env_logger::init();
 
@@ -26,26 +68,14 @@ fn main() {
     let _tcp_servers = Bastion::children(|children: Children| {
         children
             .with_redundancy(TCP_SERVER_COUNT) // Let's have 10 tcp echo servers :)
-            .with_exec(move |_ctx: BastionContext| {
-                async move {
-                    println!("Server is starting!");
-                    let port = TCP_SERVERS.fetch_sub(1, Ordering::SeqCst) + 2000;
-                    let addr = format!("127.0.0.1:{}", port);
+            .with_exec(move |_ctx: BastionContext| async move {
+                println!("Server is starting!");
+                let port = TCP_SERVERS.fetch_sub(1, Ordering::SeqCst) + 2000;
+                let addr = format!("127.0.0.1:{}", port);
 
-                    let listener = Handle::<TcpListener>::bind(addr).unwrap();
-                    println!("Listening on {}", listener.get_ref().local_addr().unwrap());
+                run(addr);
 
-                    // Accept clients in a loop.
-                    loop {
-                        let (stream, peer_addr) = listener.accept().await.unwrap();
-                        println!("Accepted client: {}", peer_addr);
-
-                        // Spawn a task that echoes messages from the client back to it.
-                        spawn(echo(stream));
-                    }
-
-                    Ok(())
-                }
+                Ok(())
             })
     })
     .expect("Couldn't start a new children group.");
