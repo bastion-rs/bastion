30be6b621bb7f04a2c097b43c998ed8cbfda3677
o0Ignition0o <jeremy.lempereur@gmail.com>
2020-08-13 02:10:49 +0200

    lets see if lock happens

--- src/bastion-executor/src/load_balancer.rs
+++ src/bastion-executor/src/load_balancer.rs
@@ -89,7 +89,7 @@ impl LoadBalancer {
 
     pub fn park_thread(&self, thread: Thread) {
         self.parked_threads
-            .try_lock()
+            .lock()
             .or_else(|e| {
                 warn!("Bastion-executor: park_thread: couldn't lock parked_threads - {}", e);
                 Err(e)
@@ -102,7 +102,7 @@ impl LoadBalancer {
 
     pub fn unpark_thread(&self) {
         self.parked_threads
-            .try_lock()
+            .lock()
             .or_else(|e| {
                 warn!("Bastion-executor: unpark_thread: couldn't lock parked_threads - {}", e);
                 Err(e)
