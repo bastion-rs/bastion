8b94420fe01eaacfe68336ae46c3710007b6ab80
Nicolai Unrein <n.unrein@gmail.com>
2020-08-20 11:07:19 +0200

    add tests for restart_strategy delay calculation

--- src/bastion/tests/restart_strategy.rs
+++ src/bastion/tests/restart_strategy.rs
@@ -1,5 +1,5 @@
 use bastion::supervisor::{ActorRestartStrategy, RestartPolicy, RestartStrategy};
-use std::time::Duration;
+use std::time::{Duration, Instant};
 
 #[test]
 fn check_default_values() {
@@ -42,4 +42,62 @@ fn override_restart_strategy_and_policy() {
 
     assert_eq!(restart_strategy.restart_policy(), policy);
     assert_eq!(restart_strategy.strategy(), strategy);
+}
+
+#[test]
+fn calculate_immediate_strategy() {
+    let strategy = ActorRestartStrategy::Immediate;
+
+    assert_eq!(strategy.calculate(0), None);
+    assert_eq!(strategy.calculate(1), None);
+    assert_eq!(strategy.calculate(100), None);
+}
+
+#[test]
+fn calculate_linear_strategy() {
+    let strategy = ActorRestartStrategy::LinearBackOff {
+        timeout: Duration::from_millis(100),
+    };
+
+    assert_eq!(strategy.calculate(0), Some(Duration::from_millis(100)));
+
+    assert_eq!(
+        strategy.calculate(1),
+        Some(Duration::from_millis(100 + 1 * 100))
+    );
+    assert_eq!(
+        strategy.calculate(99),
+        Some(Duration::from_millis(100 + 99 * 100))
+    );
+}
+
+#[test]
+fn calculate_exp_strategy_with_multiplier_zero() {
+    let strategy = ActorRestartStrategy::ExponentialBackOff {
+        timeout: Duration::from_millis(100),
+        multiplier: 0,
+    };
+
+    assert_eq!(strategy.calculate(0), Some(Duration::from_millis(100)));
+    assert_eq!(strategy.calculate(1), Some(Duration::from_millis(100)));
+    assert_eq!(strategy.calculate(100), Some(Duration::from_millis(100)));
+}
+
+#[test]
+fn calculate_exp_strategy_with_multiplier_non_zero() {
+    let strategy = ActorRestartStrategy::ExponentialBackOff {
+        timeout: Duration::from_millis(100),
+        multiplier: 5,
+    };
+
+    assert_eq!(strategy.calculate(0), Some(Duration::from_millis(100)));
+
+    assert_eq!(
+        strategy.calculate(1),
+        Some(Duration::from_millis(100 + 1 * 5 * 100))
+    );
+    assert_eq!(
+        strategy.calculate(99),
+        Some(Duration::from_millis(100 + 99 * 5 * 100))
+    );
 }
