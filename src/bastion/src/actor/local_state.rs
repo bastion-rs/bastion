4be0cf3528fab5724297207cb2d17ebf21e03c14
Mahmut Bulut <vertexclique@gmail.com>
2020-12-02 10:13:16 +0100

    Adjust local state

--- src/bastion/src/actor/local_state.rs
+++ src/bastion/src/actor/local_state.rs
@@ -6,73 +6,95 @@ use std::any::{Any, TypeId};
 use std::sync::Arc;
 
 use lever::table::lotable::LOTable;
+use std::borrow::BorrowMut;
+use std::borrow::Borrow;
+use std::convert::identity;
+use std::collections::HashMap;
 
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 /// A unified storage for actor's data and intended to use
 /// only in the context of the single actor.
 pub(crate) struct LocalState {
-    table: LOTable<TypeId, LocalDataContainer>,
+    table: HashMap<TypeId, LocalDataContainer>,
 }
 
-#[derive(Debug, Clone)]
-/// Wrapper for the `Arc<AtomicBox<Box<T>>` type that provides
+#[derive(Debug)]
+#[repr(transparent)]
+/// Transparent type for the `Box<dyn Any + Send + Sync>` type that provides
 /// simpler and easier to use API to developers.
-pub struct LocalDataContainer(Arc<Box<dyn Any + Send + Sync + 'static>>);
+pub struct LocalDataContainer(Box<dyn Any + Send + Sync>);
 
 impl LocalState {
     /// Returns a new instance of local state for actor.
     pub(crate) fn new() -> Self {
         LocalState {
-            table: LOTable::new(),
+            table: HashMap::with_capacity(1 << 10),
         }
     }
 
     /// Inserts the given value in the table. If the value
     /// exists, it will be overridden.
-    pub fn insert<T: Send + Sync + 'static>(&self, value: T) {
+    pub fn insert<T: Send + Sync + 'static>(&mut self, value: T) {
         let container = LocalDataContainer::new(value);
-        self.table.insert(TypeId::of::<T>(), container).ok();
+        self.table.insert(TypeId::of::<T>(), container);
     }
 
     /// Checks the given values is storing in the table.
     pub fn contains<T: Send + Sync + 'static>(&self) -> bool {
-        match self.table.get(&TypeId::of::<T>()) {
-            Some(_) => true,
-            _ => false,
-        }
+        self.table.contains_key(&TypeId::of::<T>())
     }
 
-    /// Returns local data container to the caller if it exists.
-    pub fn get_container<T: Send + Sync + 'static>(&self) -> Option<LocalDataContainer> {
-        self.table
-            .get(&TypeId::of::<T>())
-            .and_then(|data| Some(data))
+    /// Runs given closure on the state
+    pub fn with_state<F, R, T: Send + Sync + 'static>(&self, f: F) -> Option<R>
+    where
+        F: FnOnce(Option<&T>) -> Option<R>
+    {
+        self.table.get(&TypeId::of::<T>())
+            .and_then(|e| f(e.get()))
     }
 
     /// Deletes the entry from the table.
-    pub fn remove<T: Send + Sync + 'static>(&self) -> bool {
-        match self.table.remove(&TypeId::of::<T>()) {
-            Ok(_) => true,
-            Err(_) => false,
-        }
+    pub fn remove<T: Send + Sync + 'static>(&mut self) -> bool {
+        self.table.remove(&TypeId::of::<T>()).is_some()
+    }
+
+    /// Returns immutable data to the caller.
+    pub fn get<'a, T: Send + Sync + 'static>(&'a self) -> Option<&'a T> {
+        self.get_container::<T>().and_then(|e| e.0.downcast_ref())
+    }
+
+    /// Returns mutable data to the caller.
+    pub fn get_mut<'a, T: Send + Sync + 'static>(&'a mut self) -> Option<&'a mut T> {
+        self.get_container_mut::<T>().and_then(|e| e.0.downcast_mut())
+    }
+
+    /// Returns local data container to the caller if it exists.
+    fn get_container<T: Send + Sync + 'static>(&self) -> Option<&LocalDataContainer> {
+        self.table
+            .get(&TypeId::of::<T>())
+    }
+
+    /// Returns local data container to the caller if it exists.
+    fn get_container_mut<T: Send + Sync + 'static>(&mut self) -> Option<&mut LocalDataContainer> {
+        self.table
+            .get_mut(&TypeId::of::<T>())
     }
 }
 
 impl LocalDataContainer {
     pub(crate) fn new<T: Send + Sync + 'static>(value: T) -> Self {
-        LocalDataContainer(Arc::new(Box::new(value)))
+        LocalDataContainer(Box::new(value))
     }
 
     /// Returns immutable data to the caller.
-    pub fn get<'a, T: Send + Sync + 'static>(&'a self) -> Option<&'a T> {
+    fn get<'a, T: Send + Sync + 'static>(&'a self) -> Option<&'a T> {
         self.0.downcast_ref()
     }
 
-    // FIXME: Find a way to return mutable reference instead of usage the insert() call
-    // /// Returns mutable data to the caller.
-    // pub fn get_mut<'a, T: Send + Sync + 'static>(&'a mut self) -> Option<&'a mut T> {
-    //     Arc::get_mut(&mut self.0).and_then(|data| data.downcast_mut())
-    // }
+    /// Returns mutable data to the caller.
+    fn get_mut<'a, T: Send + Sync + 'static>(&'a mut self) -> Option<&'a mut T> {
+        self.0.downcast_mut()
+    }
 }
 
 #[cfg(test)]
@@ -86,7 +108,7 @@ mod tests {
 
     #[test]
     fn test_insert() {
-        let instance = LocalState::new();
+        let mut instance = LocalState::new();
 
         instance.insert(Data { counter: 0 });
         assert_eq!(instance.contains::<Data>(), true);
@@ -94,7 +116,7 @@ mod tests {
 
     #[test]
     fn test_insert_with_duplicated_data() {
-        let instance = LocalState::new();
+        let mut instance = LocalState::new();
 
         instance.insert(Data { counter: 0 });
         assert_eq!(instance.contains::<Data>(), true);
@@ -112,7 +134,7 @@ mod tests {
 
     #[test]
     fn test_get_container() {
-        let instance = LocalState::new();
+        let mut instance = LocalState::new();
 
         let expected = Data { counter: 1 };
 
@@ -130,38 +152,35 @@ mod tests {
 
     #[test]
     fn test_get_container_with_mutable_data() {
-        let instance = LocalState::new();
+        let mut instance = LocalState::new();
 
-        let expected = Data { counter: 1 };
+        let mut expected = Data { counter: 1 };
 
         instance.insert(expected.clone());
         assert_eq!(instance.contains::<Data>(), true);
 
         // Get the current snapshot of data
-        let result_get = instance.get_container::<Data>();
-        assert_eq!(result_get.is_some(), true);
-
-        let mut container = result_get.unwrap();
-        let data = container.get::<Data>();
+        let mut data = instance.get_mut::<Data>();
         assert_eq!(data.is_some(), true);
-        assert_eq!(data.unwrap(), &expected);
+        assert_eq!(data, Some(&mut expected));
+
+        data.map(|d| {
+            d.counter += 1;
+            d
+        });
 
         // Replace the data onto new one
         let expected_update = Data { counter: 2 };
-        instance.insert(expected_update.clone());
 
         // Check the data was updated
-        let updated_result_get = instance.get_container::<Data>();
-        assert_eq!(updated_result_get.is_some(), true);
-        let mut updated_container = updated_result_get.unwrap();
-        let result_updated_data = updated_container.get::<Data>();
+        let result_updated_data = instance.get::<Data>();
         assert_eq!(result_updated_data.is_some(), true);
         assert_eq!(result_updated_data.unwrap(), &expected_update);
     }
 
     #[test]
     fn test_get_container_returns_none() {
-        let instance = LocalState::new();
+        let mut instance = LocalState::new();
 
         let container = instance.get_container::<usize>();
         assert_eq!(container.is_none(), true);
@@ -169,7 +188,7 @@ mod tests {
 
     #[test]
     fn test_remove_returns_true() {
-        let instance = LocalState::new();
+        let mut instance = LocalState::new();
 
         instance.insert(Data { counter: 0 });
         assert_eq!(instance.contains::<Data>(), true);
@@ -180,9 +199,9 @@ mod tests {
 
     #[test]
     fn test_remove_returns_false() {
-        let instance = LocalState::new();
+        let mut instance = LocalState::new();
 
         let is_removed = instance.remove::<usize>();
-        assert_eq!(is_removed, true);
+        assert_eq!(is_removed, false);
     }
 }
