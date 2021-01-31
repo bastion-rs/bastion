affa0fe09b53da98d24c414dbcc92d963334dd5d
o0Ignition0o <jeremy.lempereur@gmail.com>
2021-01-31 14:56:58 +0100

    use tokio::main!

--- src/bastion/examples/hello_tokio.rs
+++ src/bastion/examples/hello_tokio.rs
@@ -1,3 +1,4 @@
+use anyhow::Result as AnyResult;
 use bastion::prelude::*;
 #[cfg(feature = "runtime-tokio")]
 use tokio;
@@ -11,14 +12,21 @@ use tracing::{error, info, warn, Level};
 ///
 /// Log output:
 ///
-/// Jan 31 11:54:14.372 ERROR hello_tokio: just received hello, world!
-/// Jan 31 11:54:14.472  WARN hello_tokio: awaited 5 seconds
-/// Jan 31 11:54:14.472 ERROR hello_tokio: waited for the blocking! to be complete!
-/// Jan 31 11:54:14.473 ERROR hello_tokio: not waiting for spawn! to be complete, moving on!
-/// Jan 31 11:54:24.473  WARN hello_tokio: the spawn! is complete
-/// Jan 31 11:54:34.372  WARN hello_tokio: we're done, stopping the bastion!
+/// Jan 31 14:55:55.677  WARN hello_tokio: just spawned!
+/// Jan 31 14:55:56.678  WARN hello_tokio: Ok let's handle a message now.
+/// Jan 31 14:55:56.678 ERROR hello_tokio: just received hello, world!
+/// Jan 31 14:55:56.678  WARN hello_tokio: sleeping for 2 seconds without using the bastion executor
+/// Jan 31 14:55:58.680  WARN hello_tokio: and done!
+/// Jan 31 14:55:58.681  WARN hello_tokio: let's sleep for 5 seconds within a blocking block
+/// Jan 31 14:56:03.682  WARN hello_tokio: awaited 5 seconds
+/// Jan 31 14:56:03.682 ERROR hello_tokio: waited for the blocking! to be complete!
+/// Jan 31 14:56:03.682 ERROR hello_tokio: not waiting for spawn! to be complete, moving on!
+/// Jan 31 14:56:03.683  WARN hello_tokio: let's sleep for 10 seconds within a spawn block
+/// Jan 31 14:56:13.683  WARN hello_tokio: the spawn! is complete
+/// Jan 31 14:56:15.679  WARN hello_tokio: we're done, stopping the bastion!
 #[cfg(feature = "runtime-tokio")]
-fn main() {
+#[tokio::main]
+async fn main() -> AnyResult<()> {
     // Initialize tracing logger
     // so we get nice output on the console.
     let subscriber = tracing_subscriber::fmt()
@@ -31,14 +39,22 @@ fn main() {
     let workers = Bastion::children(|children| {
         children.with_exec(|ctx: BastionContext| {
             async move {
+                warn!("just spawned!");
+                tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+                warn!("Ok let's handle a message now.");
                 msg! {
                     ctx.recv().await?,
                     msg: &'static str => {
                         // Printing the incoming msg
                         error!("just received {}", msg);
 
+                        warn!("sleeping for 2 seconds without using the bastion executor");
+                        tokio::time::sleep(std::time::Duration::from_secs(2)).await;
+                        warn!("and done!");
+
                         // let's wait until a tokio powered future is complete
                         run!(blocking! {
+                            warn!("let's sleep for 5 seconds within a blocking block");
                             tokio::time::sleep(std::time::Duration::from_secs(5)).await;
                             warn!("awaited 5 seconds");
                         });
@@ -46,6 +62,7 @@ fn main() {
 
                         // let's spawn a tokio powered future and move on
                         spawn! {
+                            warn!("let's sleep for 10 seconds within a spawn block");
                             tokio::time::sleep(std::time::Duration::from_secs(10)).await;
                             warn!("the spawn! is complete");
                         };
@@ -75,6 +92,7 @@ fn main() {
 
     // We are done, stopping the bastion!
     Bastion::stop();
+    Ok(())
 }
 
 #[cfg(not(feature = "runtime-tokio"))]
